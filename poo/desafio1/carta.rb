5a559350e6e1353755218ad4939388071cc4b421
alvaro <alvaroriquelme@live.cl>
2020-10-03 15:35:25 -0300

    1st

---
+++ poo/desafio1/carta.rb
@@ -0,0 +1,19 @@
+class Carta
+    attr_accessor :numero
+    attr_accessor :pinta
+    def initialize (numero, pinta)
+        @numero = numero
+        @pinta = pinta
+    end
+end
+
+pintas = ['C','D','E','T']
+mano = []
+
+5.times do |i|
+    carta_azar = Carta.new(Random.rand(13), pintas.sample)
+    carta_azar = [carta_azar.numero,carta_azar.pinta]
+    mano.push(carta_azar)
+end
+
+puts mano
